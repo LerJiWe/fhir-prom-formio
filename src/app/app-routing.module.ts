3b79785eeffa73908777c63b674fbbfde10f4613
LerJiWe <lerjiwe@gmail.com>
2026-01-23 21:05:21 +0800

    fix: 修正改成預設路徑到 launch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LaunchComponent } from './fhir/launch/launch.component';
 
 const routes: Routes = [
-  { path: 'launch', component: LaunchComponent }, // 處理啟動
   // { path: 'index', component: AppComponent },      // 處理回調 (或你的主畫面)
   // { path: '', redirectTo: 'index', pathMatch: 'full' }
-  { path: '', redirectTo: 'fhir', pathMatch: 'full' },
+  { path: '', redirectTo: 'launch', pathMatch: 'full' },
+  { path: 'launch', component: LaunchComponent }, // 處理啟動
   {
     path: 'fhir',
     component: FhirComponent,
--- src/app/fhir/launch/launch.component.ts
+++ src/app/fhir/launch/launch.component.ts
@@ -13,28 +13,32 @@ export class LaunchComponent implements OnInit {
   ngOnInit(): void {
 
     // 檢查 URL 中是否存在 iss 參數（無論是在 # 之前還是之後）
-    const urlParams = new URLSearchParams(window.location.search);
-    const iss = urlParams.get("iss");
-    const launch = urlParams.get("launch");
+    // const urlParams = new URLSearchParams(window.location.search);
+    // const iss = urlParams.get("iss");
+    // const launch = urlParams.get("launch");
 
-    if (iss && launch) {
-      // 如果在網址列抓到了 iss，手動傳給 authorize
-      FHIR.oauth2.authorize({
-        clientId: 'my_web_app',
-        scope: 'launch openid fhirUser patient/*.read',
-        redirectUri: 'fhir-prom-formio/fhir',
-        iss: iss,
-        launch: launch
-      });
-    } else {
-      // 如果沒抓到，嘗試讓套件自己抓（原本的邏輯）
-      FHIR.oauth2.authorize({
-        clientId: 'my_web_app',
-        scope: 'launch openid fhirUser patient/*.read'
-      }).catch(err => console.error(err));
-    }
+    // if (iss && launch) {
+    //   // 如果在網址列抓到了 iss，手動傳給 authorize
+    //   FHIR.oauth2.authorize({
+    //     clientId: 'my_web_app',
+    //     scope: 'launch openid fhirUser patient/*.read',
+    //     redirectUri: 'fhir-prom-formio/fhir',
+    //     iss: iss,
+    //     launch: launch
+    //   });
+    // } else {
+    //   // 如果沒抓到，嘗試讓套件自己抓（原本的邏輯）
+    //   FHIR.oauth2.authorize({
+    //     clientId: 'my_web_app',
+    //     scope: 'launch openid fhirUser patient/*.read'
+    //   }).catch(err => console.error(err));
+    // }
 
-
+    FHIR.oauth2.authorize({
+      clientId: 'my_web_app',
+      scope: 'launch openid fhirUser patient/*.read',
+      redirectUri: 'fhir-prom-formio/fhir'
+    });
 
   }
 
